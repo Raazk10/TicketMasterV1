a5a1e528c286a9e70a0211b27ab88a7587fb25ee
Raazk10 <rakioraag10@gmail.com>
2023-03-06 13:39:39 +0100

    renamed class

--- js/modules/fetchEventSearch.js
+++ js/modules/fetchEventSearch.js
@@ -1,10 +1,8 @@
-const testurl =
-  "https://app.ticketmaster.com/discovery/v2/events/Z698xZb_Z17jo84?apikey=aLGf7LIEfrlaEtpVXyO5Oj9InyDNM8gN&locale=*";
 const apiKey = "aLGf7LIEfrlaEtpVXyO5Oj9InyDNM8gN";
-const baseUrl = `https://app.ticketmaster.com/discovery/v2/events.json?&apikey=${apiKey}&locale=*&keyword=`;
+const baseUrl = `https://app.ticketmaster.com/discovery/v2/events.json?&apikey=${apiKey}&locale=*&keyword=&city=`;
 
-export default async function fetchEventSearch(query) {
-  const response = await fetch(`${baseUrl}${query}`);
+export default async function fetchEventSearch(cityName) {
+  const response = await fetch(`${baseUrl}${cityName}`);
   const result = await response.json();
 
   const events = result._embedded?.events?.map((event) => {
