5560c662245e9f4ca85f8f393935ea49e0c66910
Raazk10 <rakioraag10@gmail.com>
2023-03-06 13:38:54 +0100

    imported eventinformation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import Header from "../js/modules/header.js";
 import handleSearchEvent from "../js/modules/handleSearchEvent.js";
 import getEventDetails from "./modules/getEventDetails.js";
-
+import eventInformation from "./modules/eventInformation.js";
+eventInformation();
 Header();
+
 getEventDetails();
 handleSearchEvent();
