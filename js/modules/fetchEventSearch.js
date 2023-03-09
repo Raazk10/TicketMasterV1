5f9c023912027041649be1bfb41921127917ca8d
Raazk10 <rakioraag10@gmail.com>
2023-03-09 19:52:04 +0100

    added error handling

--- js/modules/fetchEventSearch.js
+++ js/modules/fetchEventSearch.js
@@ -2,25 +2,46 @@ import { clientId } from "../env.js";
 const url = `https://app.ticketmaster.com/discovery/v2/events.json?&apikey=${clientId}&size=50&locale=*&keyword=`;
 
 export default async function fetchEventSearch(query) {
-  const response = await fetch(`${url}${query}`);
-  const result = await response.json();
-  console.log(result);
+  try {
+    const response = await fetch(`${url}${query}`);
+    const events = await handleResponse(response);
+    if (events.length === 0) {
+      return null;
+    }
+    return events;
+  } catch (error) {
+    handleError(error);
+  }
+}
 
-  /* try{
-    await handleResponse(response);
-  }catch(error){
-   handleError(error)
-  } */
-
-  const events = result._embedded?.events?.map((event) => {
-    return {
-      name: event.name,
-      id: event.id,
-      venue: event._embedded?.venues?.[0]?.name,
-      city: event._embedded?.venues?.[0].city?.name,
-      img: event.images?.[0]?.url,
-      date: event.dates?.start,
-    };
-  });
-  return events;
+async function handleResponse(response) {
+  if (response.ok) {
+    const result = await response.json();
+    console.log(result);
+    return result._embedded?.events?.map((event) => {
+      return {
+        name: event.name,
+        id: event.id,
+        venue: event._embedded?.venues?.[0]?.name,
+        city: event._embedded?.venues?.[0].city?.name,
+        img: event.images?.[0]?.url,
+        date: event.dates?.start,
+      };
+    });
+  } else if (response.status === 400) {
+    throw new Error("Something wrong with api url");
+  } else if (response.status === 404) {
+    throw new Error("Url not existing");
+  } else if (response.status === 401) {
+    throw new Error("Not authorized user");
+  } else if (response.status >= 500) {
+    throw new Error("Server not responding");
+  } else {
+    throw new Error("No events found try again");
+  }
+}
+export function handleError(error) {
+  const warningElement = document.querySelector(".warning");
+  warningElement.classList.remove("hidden");
+  warningElement.textContent = error.message;
 }
--- js/modules/handleSearchEvent.js
+++ js/modules/handleSearchEvent.js
@@ -1,10 +1,12 @@
 import fetchEventSearch from "./fetchEventSearch.js";
 import renderSearchEvent from "./renderSearchEvent.js";
+import { handleError } from "./fetchEventSearch.js";
 
 export default function handleSearchEvent() {
   // qyery selector
   const searchInput = document.querySelector(".search__input");
   const searchForm = document.querySelector(".search");
+  const warningElement = document.querySelector(".warning");
 
   //event listener
   searchForm.addEventListener("submit", handleSearch);
@@ -13,9 +15,15 @@ export default function handleSearchEvent() {
   async function handleSearch(e) {
     e.preventDefault();
     const query = searchInput.value;
-    const searchedValue = await fetchEventSearch(query);
 
-    renderSearchEvent(searchedValue);
-    searchInput.value = "";
+    const searchedValue = await fetchEventSearch(query);
+    if (searchedValue) {
+      warningElement.classList.add("hidden"); // Hide warning message
+      warningElement.textContent = ""; // Clear warning message
+      renderSearchEvent(searchedValue);
+      searchInput.value = "";
+    } else {
+      handleError(error);
+    }
   }
 }
