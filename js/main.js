d8383e96c0b5905ae08a552bd963ab78f28a0e45
Raazk10 <rakioraag10@gmail.com>
2023-05-31 12:44:07 +0200

    Import randomImage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import Header from "../js/modules/header.js";
 import handleSearchEvent from "../js/modules/handleSearchEvent.js";
 import getEventDetails from "./modules/getEventDetails.js";
+import getRandomImages from "./modules/getRandomImage.js";
 
 getEventDetails();
 handleSearchEvent();
 Header();
+getRandomImages();
